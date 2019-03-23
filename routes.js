92f4b72909ef60adcf8da19af50bbf9276a77075
Sajal Arora <sajalarora007@gmail.com>
2019-03-23 18:01:12 +0530

    added new routes and secured unhandled endpoints

--- routes.js
+++ routes.js
@@ -1,3 +1,19 @@
 module.exports = (app) => {
-    app.use('/user', require('./apis/users'));
-}   
+    app.use('/api', require('./apis/users'));
+
+    // Error handler for all the bad requests...
+    app.use((req, res, next) => {
+        const error = new Error('Not Found');
+        error.status = 404;
+        next(error);
+      });
+  
+      app.use((error, req, res, next) => {
+        res.status(error.status || 500);
+        res.json({
+          error: {
+            message: error.message
+          }
+        });
+      });
+}
